f6b8d825b3ebc3188c884f047b35e7230a2a3429
groscoe <gustavo.roscoe@waycarbon.com>
2018-02-17 15:19:48 -0200

    Added module to parse bovespa-style record files

---
+++ read_bovespa.py
@@ -0,0 +1,89 @@
+'''
+read_bovespa.py - helper functions to interpret records from
+BOVESPA's historical data.
+
+obs: data available in http://http://www.bmfbovespa.com.br
+'''
+import pandas as pd
+from collections import OrderedDict
+from datetime import date
+from datetime import datetime as dt
+from typing import Callable, Iterator, Union, Dict
+
+
+RecordTypes = Union[int, str, float, date]
+FilePath = str
+CharStream = Iterator[str]
+BovespaDateString = str
+
+
+def read_bovespa_file(filename: FilePath) -> pd.DataFrame:
+    '''read a file in the BOVESPA historical records format as a
+    Pandas' DataFrame.'''
+    records = []
+    with open(filename, 'r') as bovespa_file:
+        bovespa_file.readline()  # skip file header
+        for line in bovespa_file:
+            try:
+                records.append(read_bovespa_record(line))
+            except ValueError:  # skip file footer
+                break
+
+    return pd.DataFrame(records)
+
+
+def read_bovespa_record(line: str) -> Dict[str, RecordTypes]:
+    '''read a line from a BOVESPA style record as a dictionary.'''
+    read = read_from(line)
+    skip = read  # just for clarity
+    record: Dict[str, RecordTypes] = OrderedDict()
+
+    skip(2)  # register type code (always "01")
+    record["date"] = read_date(read(8))
+    skip(2)  # BDI code
+    record["symbol"] = read(12)
+    skip(3)  # market type code
+    record["company_name"] = read(12)
+    record["type"] = read(10)
+    skip(3)  # market time in days
+    record["currency"] = read(4)
+    record["open"] = read_float(read(13))
+    record["high"] = read_float(read(13))
+    record["low"] = read_float(read(13))
+    record["mean"] = read_float(read(13))
+    record["close"] = read_float(read(13))
+    skip(13)  # best buy offer price
+    skip(13)  # best sell offer price
+    skip(5)  # number of exchanges for the asset
+    record["quantity"] = int(read(18))
+    record["volume"] = read_float(read(18))
+    record["option_strike_price"] = read_float(read(13))
+    skip(1)  # price correction indicator
+    record["option_expiry_date"] = read_date(read(8))
+
+    return record
+
+
+def read_from(string: str) -> Callable[[int], str]:
+    '''return a function to read a string as a stream.'''
+    it: CharStream = iter(string)
+    return lambda n: read_n(n, it)
+
+
+def read_n(n: int, iterator: CharStream) -> str:
+    '''read n chars from a string iterator, join them and strip whitespace.'''
+    return ''.join([next(iterator, '') for _ in range(n)]).strip()
+
+
+def read_date(date_string: BovespaDateString) -> date:
+    '''read a string as a date in "YYYYMMDD" format.'''
+    date_format = "%Y%m%d"  # from http://strftime.org/
+    return dt.strptime(date_string, date_format).date()
+
+
+def read_float(value: str, after_comma: int = 2) -> float:
+    '''read a float from a string of integers with decimal place metadata.'''
+    integral_part = value[:-after_comma]
+    fractional_part = value[-after_comma:]
+
+    return float(f'{integral_part}.{fractional_part}')
